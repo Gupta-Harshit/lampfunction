7ea15ef4c42fa510be4b978b04b77e86b53432d1
Nitin Garg <nitin992503@gmail.com>
2019-06-21 00:22:33 +0530

    done

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,33 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
- exports.hey = functions.https.onRequest((request, response) => {
+let lampRef = admin.database().ref('/lamp');
+let lamp1   = lampRef.child('lamp1');
+let lamp2   = lampRef.child('lamp2');
 
-         admin
-        .database()
-        .ref('/call')
-             .push('2')
-        .then(() =>
-            response.send("Waiting")
-         )
-        .catch(() =>
-            response.send("oops")
-         )
- });
+exports.hey = functions.https.onRequest((request, response) => {
+    lamp1.update({
+        color  : request.query.color,
+        status : request.query.status 
+    })
+    .then((dataSnapshot) =>{
+        response.send({status : 200 , data : dataSnapshot.ref.toString()});
+    })
+    .catch((err) =>{
+        response.send({status : 500 , message : err});
+    })
+});
+
+/*
+    lamp1.once("value")
+    .then(doc=>{
+        let obj = doc.toJSON();
+        console.log(obj);
+        if(obj.status == "ON"){
+            //turn on lamp 2
+        }
+    })
+
 //exports.areyoucallingme = functions.http.onRequest((request, response) => {
 
 //    admin
@@ -22,3 +36,5 @@ admin.initializeApp();
 //    .ref('/call')
 //        const .pop()
 //})
+
+*/
